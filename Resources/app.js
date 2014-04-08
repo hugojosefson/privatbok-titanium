c43f5977a3edf80d40b0a189852f10338984aa04
Hugo Josefson <hugo@josefson.org>
2014-04-08 21:10:18 +0200

    Wrap privatbok instead of default stuff.

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,20 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
+var webview = Titanium.UI.createWebView({url: 'https://privatbok.meteor.com/'});
+var window = Titanium.UI.createWindow();
+function printChildren() {
+    console.log(webview.children, webview.url);
+    setTimeout(printChildren, 1000);
+}
+setTimeout(printChildren, 1000);
 
-
-//
-// create base UI tab and root window
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
+webview.on('beforeload', function (event) {
+    console.log('beforeload: navigationType=' + event.navigationType + '; url=' + event.url);
 });
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
+window.add(webview);
+window.open();
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+setTimeout(function () {
+    webview.evalJS('var oldOpen = window.open; window.open = function(){console.log("OPENING!!"); oldOpen.apply(this, arguments);}');
+}, 5000);
